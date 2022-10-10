46cad96ef293bebd643f56ff7a6e80d9e3aee387
Abhishek <abhishekparmar162@gmail.com>
2022-10-10 23:22:39 +0530

    added navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1 className='bg-red-50 font-Mochiy mx-6 md:max-w-2xl md:mx-auto'>NavBar</h1></div>
+  )
+}
+
+export default NavBar
