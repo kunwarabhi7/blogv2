5936de6d954df120624a00f35652ee49f077048b
Abhishek <abhishekparmar162@gmail.com>
2022-10-11 23:50:35 +0530

    added google button

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {FcGoogle} from 'react-icons/fc'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div className='shadow-xl p-16 mt-12 text-gray-800 ' >
+        <h1 className='text-2xl font-medium'>Join Now</h1>
+    <div className='py-4'>
+        <h2 className='py-4'>Sign In with our provider</h2>
+        <button className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'>Sign in with GOOGLE <FcGoogle className='text-2xl' /></button>
+        
+    </div>
+    </div>
   )
 }
 
