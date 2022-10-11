8a5ab62c4ba35328e1e15ffe457aef0e05dbf8af
Abhishek <abhishekparmar162@gmail.com>
2022-10-12 00:03:50 +0530

    remove extra code

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,4 +1,3 @@
-
 import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 import {FcGoogle} from 'react-icons/fc'
@@ -12,23 +11,14 @@ const login = () => {
 
     const signInWithGooglePopup = () => {
 signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
+.then((result) => {
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
+    
     const user = result.user;
     console.log(user)
-    // ...
   }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
+    console.log(error.message)
   });
 
     }
