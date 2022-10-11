385c2289ef1ce1ddea059acca6e96628e06cf3fe
Abhishek <abhishekparmar162@gmail.com>
2022-10-11 23:32:30 +0530

    fix navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
 <div className="font-Mochiy text-lg bg-cyan-300 p-2 rounded-md m-2 cursor-pointer">Home</div>
       </Link>
       <Link href={'/auth/login'}>
-<div className='bg-cyan-300 rounded-lg p-4 m-4 '><button>Sign IN</button> </div>
+<div className='bg-cyan-300 rounded-lg px-4 py-2 m-4 '><button>Join Now</button> </div>
       </Link>
 </div>
   )
