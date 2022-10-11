46890e3e2f83145a0b4fab8626b061c2e61704c0
Abhishek <abhishekparmar162@gmail.com>
2022-10-11 23:57:52 +0530

    added google login functionality

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,12 +1,43 @@
+
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utils/firebase";
 import {FcGoogle} from 'react-icons/fc'
 
+
+
 const login = () => {
+    const provider = new GoogleAuthProvider();
+
+
+
+    const signInWithGooglePopup = () => {
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+    }
   return (
     <div className='shadow-xl p-16 mt-12 text-gray-800 ' >
         <h1 className='text-2xl font-medium'>Join Now</h1>
     <div className='py-4'>
         <h2 className='py-4'>Sign In with our provider</h2>
-        <button className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'>Sign in with GOOGLE <FcGoogle className='text-2xl' /></button>
+        <button onClick={signInWithGooglePopup} className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'>Sign in with GOOGLE <FcGoogle className='text-2xl' /></button>
         
     </div>
     </div>
