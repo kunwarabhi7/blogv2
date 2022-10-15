03da8ac3a87217c831186247e9bf69c2a41bafb8
Abhishek <abhishekparmar162@gmail.com>
2022-10-15 17:20:17 +0530

    added dashboard functioality

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,12 @@
 import {auth} from '../utils/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { signOut } from "firebase/auth";
 import { useRouter } from 'next/router';
-
+import { useEffect } from 'react';
 
 const dashboard = () => {
     const router = useRouter()
+    const [user , loader] = useAuthState(auth)
 
     const SignOutUser = () => {
         signOut(auth).then(() => {
@@ -14,6 +16,16 @@ const dashboard = () => {
             // An error happened.
           });
     }
+const getData = () =>{
+
+    if(loader) return ;
+    if(!user) return router.push('/auth/login')
+}
+
+useEffect(()=>{
+    getData();
+},[user,loader])
+
   return (
     <div>
         <h1>Your Posts</h1>
