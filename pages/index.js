d835129e443f4d8218ca903a7f9268a4609fd532
Abhishek <abhishekparmar162@gmail.com>
2022-10-17 17:15:12 +0530

    added home page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,19 @@
 import Head from 'next/head'
 import PostBody from '../components/PostBody'
 import { useState,useEffect } from 'react'
-import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import {  onSnapshot, orderBy, query } from "firebase/firestore";
 import {db, postsRef} from '../utils/firebase'
 
 export default function Home() {
 const [allData , setAllData]  = useState([])
 
   const getAllPost =async () => {
-    const querySnapshot = await getDocs(collection(db, "posts"));
     const q = query(postsRef, orderBy('timestamp','desc'))
 
     const unsubsribe = onSnapshot(q, (snapshot)=>{
       setAllData(snapshot.docs.map((doc)=> ({...doc.data() , id: doc.id})));
     })
 
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  setAllData(doc.id, " => ", doc.data());
-});
 return unsubsribe;
   }
 
@@ -34,13 +29,10 @@ return unsubsribe;
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="my-12 text-lg font-medium">
-        <h2>See what other people are saying</h2>
-        {allData.map((post) => (
-          <PostBody key={post.id} {...post}>
-         
-                   </PostBody>
-        ))}
+      <div>
+{allData.map((post)=>(
+  <PostBody {...post} key={post.id} />
+))}      
       </div>
     </div>
   )
